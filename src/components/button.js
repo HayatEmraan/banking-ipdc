1e34eb612dc980fa2489c171ed41c4f6f5f22e33
nimairoy <nimairoy718@gmail.com>
2023-08-07 01:00:26 +0600

    button added

---
+++ src/components/button.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button
+        className="bg-blue-500 btn btn-primary hover:bg-blue-700 text-white"
+        type="button"
+      >
+        Button
+      </button>
+    </div>
+  );
+};
+
+export default Button;
