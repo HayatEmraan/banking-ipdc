ef9ecbdca564432645110aa262cb4411b4e1139f
nimairoy <nimairoy718@gmail.com>
2023-08-07 00:19:34 +0600

    add

---
+++ src/nimai/customerExperience.js
@@ -0,0 +1,9 @@
+const CustomerExperience = () => {
+  return (
+    <div>
+      <h2 className="text-3xl font-semibold text-red-400">Hello World</h2>
+    </div>
+  );
+};
+
+export default CustomerExperience;
---
+++ src/nimai/customerExperience/CustomerExperience.jsx
@@ -0,0 +1,19 @@
+
+
+const CustomerExperience = () => {
+    return (
+        <div className="text-center">
+            <h1 className="text-4xl text-blue-500 mt-4 font-bold capitalize">
+                Creating extraordinary customer experience
+            </h1>
+            <div className="grid md:grid-cols-4 md:gap-4">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, ea.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, ea.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, ea.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, ea.</p>
+            </div>
+        </div>
+    );
+};
+
+export default CustomerExperience;
